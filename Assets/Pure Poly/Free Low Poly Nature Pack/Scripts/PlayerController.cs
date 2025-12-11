9be31aa7459729fef8058369375842b237c7589a
MiraPhilip12 <miraphilip2012@gmail.com>
2025-12-11 23:17:39 +0200

    imported collectibles asset and configured player movement

--- Assets/Pure Poly/Free Low Poly Nature Pack/Scripts/PlayerController.cs
+++ Assets/Pure Poly/Free Low Poly Nature Pack/Scripts/PlayerController.cs
@@ -1,37 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 public class PlayerController : MonoBehaviour
 {
-    // Rigidbody of the player.
     private Rigidbody rb;
-    // Movement along X and Y axes.
+    public float speed = 10f;
+
     private float movementX;
     private float movementY;
-    // Speed at which the player moves.
-    public float speed = 0;
+
+    void Update()
+    {
+        movementX = Input.GetAxis("Horizontal");
+        movementY = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    // Variable to keep track of collected "PickUp" objects.
+    private int count;
+
+    // UI text component to display count of "PickUp" objects collected.
+    public TMP_Text countText;
     // Start is called before the first frame update.
     void Start()
+    {
+        // Get and store the Rigidbody component attached to the player.
+        rb =  GetComponent<Rigidbody>();
+        // Initialize count to zero.
+        count = 0;
+        // Update the count display.
+        SetCountText();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Check if the object the player collided with has the "PickUp" tag.
+        if (other.gameObject.CompareTag("PickUp"))
         {
-            // Get and store the Rigidbody component attached to the player.
-            rb = GetComponent<Rigidbody>();
+            // Deactivate the collided object (making it disappear).
+            other.gameObject.SetActive(false);
+            // Increment the count of "PickUp" objects collected.
+            count = count + 1;
+            // Update the count display.
+            SetCountText();
         }
-    // This function is called when a move input is detected.
-    void OnMove(InputValue movementValue)
-        {
-            // Convert the input value into a Vector2 for movement.
-            Vector2 movementVector = movementValue.Get <Vector2>();
-            // Store the X and Y components of the movement.
-            movementX = movementVector.x;
-            movementY = movementVector.y;
-        }
-    // FixedUpdate is called once per fixed frame-rate frame.
-    private void FixedUpdate()
-        {
-            // Create a 3D movement vector using the X and Y inputs.
-            Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-            // Apply force to the Rigidbody to move the player.
-            rb.AddForce(movement * speed);
-        }
+    }
+    // Function to update the displayed count of "PickUp" objects collected.
+    void SetCountText()
+    {
+        // Update the count text with the current count.
+        countText.text = "Count: " + count.ToString();
+    }
 }
